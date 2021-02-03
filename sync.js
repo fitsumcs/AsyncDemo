392a4719add5b9eeccc283a8020bdf8eac248232
Fitsum Alemu <fitsumcs@gmail.com>
2021-02-03 13:17:45 +0300

    Async Example

---
+++ sync.js
@@ -0,0 +1,21 @@
+// Sync
+let login = function(username, password) {
+    setTimeout(() => {
+
+        console.log("Data has Arrived !!!");
+
+        return {
+            username,
+            password
+        }
+
+    }, 2000)
+}
+
+console.log("Start of Program !!!");
+
+let user = login("user 1", "password");
+
+console.log(user);
+
+console.log("End of Program !!!");
